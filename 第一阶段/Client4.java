c7d8346b9dd552c98e8e0b200f6cf320cadbc8f0
Tiamoer <tiamoer@outlook.com>
2019-09-28 16:18:18 +0800

    删除文件

--- 第一阶段/Client4.java
+++ 第一阶段/Client4.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -6,12 +7,12 @@ import java.net.Socket;
 
 public class Client4 {
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket(InetAddress.getLoopbackAddress(),5000);
+        Socket socket = new Socket("172.16.1.220",5000);
         InputStream inputStream = socket.getInputStream();
-        FileOutputStream fileOutputStream = new FileOutputStream("f:\\gui.txt");
+        FileOutputStream fileOutputStream = new FileOutputStream("e:\\gui.txt");
         byte[] b = new byte[1024];
         int len = 0;
-        while ((len = inputStream.read(b)) == -1) {
+        while ((len = inputStream.read(b)) != -1) {
             fileOutputStream.write(b,0,len);
         }
         socket.close();
--- 第一阶段/wfdewf.java
+++
@@ -1,3 +0,0 @@
-public class wfdewf {
-    //dewqdewfwe
-}
