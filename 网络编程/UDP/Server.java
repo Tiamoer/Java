5d2044bac363561542dd2decf7350856d7b972e9
Tiamoer <tiamoer@outlook.com>
2019-10-08 14:10:29 +0800

    基于UDP协议的多线程通信

--- 网络编程/UDP/Server.java
+++ 网络编程/UDP/Server.java
@@ -3,7 +3,6 @@ package UDP;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.SocketException;
 
 public class Server {
     public static void main(String[] args) throws IOException {
