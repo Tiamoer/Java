9621977ca3caa1ac9629d1613067db3004cffa32
Tiamoer <tiamoer@outlook.com>
2019-10-11 11:14:57 +0800

    动态代理模式

---
+++ 代理模式/DTDL.java
@@ -0,0 +1,54 @@
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+//动态代理模式
+public class DTDL {
+    public static void main(String[] args) {
+        JS o = new MB1(100);
+        DL1 d = new DL1(o);
+        JS proxyO = (JS) Proxy.newProxyInstance(o.getClass().getClassLoader(),o.getClass().getInterfaces(),d);
+        proxyO.ride();
+        proxyO.add();
+    }
+}
+//工厂接口
+interface JS {
+    public void add();
+    public void ride();
+}
+//目标类
+class MB1 implements JS {
+    int n;
+    int add = 0;
+    int ride = 1;
+    public MB1(int n) {
+        this.n = n;
+    }
+    @Override
+    public void add() {
+        for (int i=0;i<=n;i++) {
+            add = add+i;
+        }
+        System.out.println("和为："+add);
+    }
+    @Override
+    public void ride() {
+        for (int i=1;i<=n;i++) {
+            ride = ride*i;
+        }
+        System.out.println("积为："+ride);
+    }
+}
+//动态代理类
+class DL1 implements InvocationHandler {
+    private Object target;
+    public DL1(Object target) {
+        this.target = target;
+    }
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        Object o = method.invoke(target,args);
+        return o;
+    }
+}
