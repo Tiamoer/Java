ac34f9153bc1a7332ce79ae015df8341697bb6b1
Tiamoer <tiamoer@outlook.com>
2019-10-11 11:15:23 +0800

    静态代理模式

---
+++ 代理模式/StaticDL.java
@@ -0,0 +1,38 @@
+//静态代理模式
+public class StaticDL {
+    public static void main(String[] args) {
+        DL d = new DL(new MB(100));
+        d.add();
+    }
+}
+//接口
+interface Add {
+    public void add();
+}
+//目标类
+class MB implements Add {
+    int n;
+    public MB(int n) {
+        this.n = n;
+    }
+    @Override
+    public void add() {
+        System.out.println("1~"+n+"的和为：");
+    }
+}
+//代理类
+class DL implements Add {
+    private MB m;
+    private int sum = 0;
+    public DL(MB m) {
+        this.m = m;
+    }
+    @Override
+    public void add() {
+        for (int i = 0;i<=m.n;i++) {
+            sum = sum+i;
+        }
+        m.add();
+        System.out.println(sum);
+    }
+}
