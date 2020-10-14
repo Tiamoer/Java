75d97064af2c870e7190f46b6bb6b5eb47f06c04
TiAmo <tiamoer@outlook.com>
2020-10-15 07:46:59 +0800

    FileBasicClass

---
+++ 文件操作/FileBasicObject.java
@@ -0,0 +1,11 @@
+package 文件操作;
+
+import java.io.File;
+
+// 文件对象基础操作
+public class FileBasicObject {
+    public static void main(String[] args) {
+        // 文件类对象
+        File f1 = new File("./test.txt");
+    }
+}
